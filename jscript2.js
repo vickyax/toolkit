1d45b8a842066e7022d830a6a9064dedd5b67960
vickyaxo <106917681+vickyax@users.noreply.github.com>
2023-04-30 22:24:29 +0530

    Update jscript2.js

--- jscript2.js
+++ jscript2.js
@@ -1,58 +1,61 @@
-    var yn=(document.getElementById("fname1").value).length;
-    var cn=document.getElementById("fname2").value;
-    var count=0;
-    function funct(){
-        for(var i=0;i<(document.getElementById("fname1").value).length;i++){
-            for(var j=0;j<(document.getElementById("fname2").value).length;j++){
-        if((document.getElementById("fname1").value).substring(j,j+1)===(document.getElementById("fname2").value).substring(i,i+1)){
-            count+=7;
-        }
-    }
-    if((document.getElementById("fname2").value).substring(i,i+1)=='a'||'A'){
+    function funct() {
+    const firstName = document.getElementById("fname1").value.toLowerCase();
+    const secondName = document.getElementById("fname2").value.toLowerCase();
+    let count = 0;
+  if((firstName.length-2)>secondName.length){
+    count+=10;
+  }
+  if(firstName.length<5||secondName.length<5){
+count+=20;
+  }
+    for (let i = 0; i < 6; i++) {
+      const letter = secondName[i];
+      if (firstName.includes(letter)) {
+        count += 8;
+      }
+  
+      if(secondName.includes("a")){
+        count+=7;
+      }
+      if(secondName.includes("i")){
+        count+=3;
+      }
+      if(firstName.includes("a")){
+        count+=5;
+      }
+      if(firstName.includes("i")){
+        count+=3;
+      }
+      if(secondName.includes("s")){
+        count+=3;
+      }
+      if(secondName.includes("r")){
+        count+=3;
+      }
+      if(secondName.includes("y")){
         count+=2;
+      }
     }
-    if((document.getElementById("fname2").value).substring(i,i+1)=='i'||'I'){
-        count+=1;
+    if (secondName.toLowerCase().startsWith('vignesh') || secondName.toLowerCase().startsWith('vicky')) {
+      alert("😂Please Not my Name😂😂");
+    } 
+    else if (secondName.toLowerCase().startsWith('santhiya')) {
+      alert("😂yeanda de vera yarachu ponnu try pannu😂😂");
+    } 
+    else if (secondName.toLowerCase().startsWith(firstName)) {
+      alert('😂You Love Yourself❤️❤️ \n Your ❤️Love Score is '+count+0 +'%' );
+    } 
+    else if (count > 100) {
+        alert(`Your ❤️Love Score is ${count}% \n Oh My Are you Lovers in previous life?🤭🥰💕`);
+      }
+    else if (count >= 90) {
+        alert(`Your ❤️Love Score is ${count}% \n Get Married Already🤭🥰💕`);
+      } else if (count > 70) {
+      alert(`Your ❤️Love Score is ${count}% \n You are made for each other🥰💕`);
+    } else if (count > 30 && count < 70) {
+      alert(`Your ❤️Love Score is ${count}% \n Oh My😊🥰`);
+    } else {
+      alert(`Your ❤️Love Score is ${count}%  \n😋 don't give up`);
     }
-    if((document.getElementById("fname1").value).substring(i,i+1)=='a'||'A'){
-        count+=2;
-    }
-    if((document.getElementById("fname1").value).substring(i,i+1)=='i'||'I'){
-        count+=1;
-    }
-    }
-    if((document.getElementById("fname2").value).substring(0)==='vignesh'){
-        alert("😂Please Not my Name😂😂");
-    }
-    else if((document.getElementById("fname2").value).substring(0)==='vicky'){
-        alert("😂Please Not my Name😂😂");
-    }
-    else if((document.getElementById("fname2").value).substring(0)==='sandhya'){
-        alert("😂yeanda de vera ponnu try pannu😂😂");
-    }
-    else if((document.getElementById("fname2").value).substring(0)==='sandhia'){
-        alert("😂yeanda de vera ponnu try pannu😂😂");
-    }
-    else if((document.getElementById("fname2").value).substring(0)==='sandia'){
-        alert("😂yeanda de vera ponnu try pannu😂😂");
-    }
-    else if((document.getElementById("fname2").value).substring(0)==='Sandhya'){
-        alert("😂yeanda de vera ponnu try pannu😂😂");
-    }
-    else if((document.getElementById("fname2").value).substring(0)==='Sandhia'){
-        alert("😂yeanda de vera ponnu try pannu😂😂");
-    }
-    else if(count>70){
-        alert("Your ❤️Love Score is "+count+"% \n You are made for each other🥰💕");
-    }  
-    else if(count>30&&count<70){
-        alert("Your ❤️Love Score is "+count+"% \n Oh My😊🥰");
-    }  
-    else if(count<20){
-        alert("Your ❤️Love Score is "+count+"%  \n😋 don't give up");
-    }
- else{
-    alert("Your ❤️Love Score is "+count+"%");
- }
-    }
-    
+  }
+  
